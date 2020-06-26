d86bfe25e4d36ef72069c223df343a13f4504d3f
Dhanya1234 <67151042+Dhanya1234@users.noreply.github.com>
2020-06-26 19:13:06 +0530

    Add files via upload

---
+++ assignment 8-deletion of a character.py
@@ -0,0 +1,8 @@
+def remove(string, i):  
+    a = string[ : i]  
+    b = string[i + 1: ]
+    return a + b 
+if __name__ == '__main__': 
+    string = "dhanya"
+    i = 5
+    print(remove(string, i)) 
